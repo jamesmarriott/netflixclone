4014233e5bd0bd8c1cab5dc923b9f9a410201d93
James Marriott <sawheadproductions@gmail.com>
2021-04-14 18:18:52 +0200

    Netflix Clone

---
+++ src/header/styles/header.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { Link as ReachRouterLink } from 'react-router-dom';
+
+export const Background = styled.section`
+    display: flex;
+    flex-direction: column;
+`
+
+export const Container = styled.div``;
+
+export const Link = styled.p``;
+
+export const ButtonLink = styled(ReachRouterLink)``;
