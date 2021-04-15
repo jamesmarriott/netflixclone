e5edecbcb5f98f23208297fc442641e1e45f32c1
James Marriott <sawheadproductions@gmail.com>
2021-04-15 10:41:02 +0200

    basic component setup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { GlobalStyles } from './global-styles';
 import { App } from './app';
+import { FirebaseContext } from './context/firebase';
+
+const config = {
+    apiKey: "AIzaSyBmnuAF2c-okDjRvqzIfO3B6xBXeQNmwps",
+    authDomain: "netflixclone-45266.firebaseapp.com",
+    projectId: "netflixclone-45266",
+    storageBucket: "netflixclone-45266.appspot.com",
+    messagingSenderId: "629713869844",
+    appId: "1:629713869844:web:02324d41e87e69042c5b01",
+    measurementId: "G-TYJ1JFQCE4"
+  }
+
+const firebase = window.firebase.initializeApp(config);
 
 ReactDOM.render(
     <>
@@ -10,3 +23,6 @@ ReactDOM.render(
     </>, 
     document.getElementById('root')
 );
+
+
+
--- src/pages/home.js
+++ src/pages/home.js
@@ -9,19 +9,17 @@ export default function Home() {
     return (
         <>
             <HeaderContainer>
-
                 <Feature>
                     <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
                     <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+                    <OptForm>    
+                        <OptForm.Input placeholder="Email Address" />
+                        <OptForm.Button>Try it now</OptForm.Button>
+                        <OptForm.Break />
+                        <OptForm.Text>Ready to watch? Enter your email to create or restart
+                        your membership.</OptForm.Text>
+                    </OptForm>
                 </Feature>
-
-                <OptForm>
-                    <OptForm.Input placeholder="Email Address" />
-                    <OptForm.Button>Try it now</OptForm.Button>
-                    <OptForm.Break />
-                    <OptForm.Text>Ready to watch? Enter your email to create or restart
-                    your membership.</OptForm.Text>
-                </OptForm>
             </HeaderContainer>
             
             <JumbotronContainer />
